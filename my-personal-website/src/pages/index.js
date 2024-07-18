262d55bc98beb0fbbdc228b37c2d01cc62d2f1d9
Daniel <dann12345@berkeley.edu>
2024-07-17 19:26:53 -0700

    fixed import path

--- my-personal-website/src/pages/index.js
+++ my-personal-website/src/pages/index.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
-import About from "../components/About";
-import Projects from "../components/Projects";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
+import Header from "../app/components/Header";
+import Hero from "../app/components/Hero";
+import About from "../app/components/About";
+import Projects from "../app/components/Projects";
+import Contact from "../app/components/Contact";
+import Footer from "../app/components/Footer";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const Home = () => (
